3eef11baa379d27fd2b12292d623a41bf299fb96
Arvin <Arvin@apples-MacBook-Air.local>
2024-10-24 13:26:51 +0800

    Implement drag tags from element creator to preview area

--- frontend/app/components/element-creator.tsx
+++ frontend/app/components/element-creator.tsx
@@ -1,14 +1,28 @@
+"use client";
+
 export const ElementCreator = () => {
   return (
     <aside className="w-1/4 h-full bg-indigo-800">
       <ul className="p-16 flex flex-col h-full">
-        <li className="p-6 h-full">
+        <li
+          className="p-6 h-full"
+          draggable
+          onDragStart={(e) => e.dataTransfer.setData("text/plain", "image")}
+        >
           <div className="h-full bg-yellow-500 text-black flex justify-center items-center">圖片元件</div>
         </li>
-        <li className="p-6 h-full">
+        <li
+          className="p-6 h-full"
+          draggable
+          onDragStart={(e) => e.dataTransfer.setData("text/plain", "text")}
+        >
           <div className="h-full bg-teal-500 text-black flex justify-center items-center">文字元件</div>
         </li>
-        <li className="p-6 h-full">
+        <li
+          className="p-6 h-full"
+          draggable
+          onDragStart={(e) => e.dataTransfer.setData("text/plain", "carousel")}
+        >
           <div className="h-full bg-indigo-500 text-black flex justify-center items-center">輪播元件</div>
         </li>
       </ul>
--- frontend/app/components/preview-area.tsx
+++ frontend/app/components/preview-area.tsx
@@ -1,3 +1,51 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import type { ReactNode } from "react";
+
+type Content = {
+  id: string,
+  renderer: ReactNode
+}
+type ContentsKey = "image" | "text" | "carousel" | "default"
+
+const config: { [key in ContentsKey]: ReactNode } = {
+  image: <Image
+    src="/globe.svg"
+    width={300}
+    height={300}
+    alt="default image"
+  />,
+  text: <div className="h-12 flex items-center">請點擊以編輯文字</div>,
+  carousel: <div className="h-12">carousel content</div>,
+  default: <div>目前不支援的編輯格式</div>
+}
+
 export const PreviewArea = () => {
-  return <div className="w-3/4 h-full"></div>;
+  const [contents, setContents] = useState<Content[]>([]);
+
+  return (
+    <section
+      className="w-3/4 h-full overflow-auto"
+      onDragOver={(e) => e.preventDefault()}
+      onDrop={(e) => {
+        const type = e.dataTransfer.getData("text/plain") as ContentsKey;
+        setContents((prev) => [
+          ...prev, { id: uuidv4(), renderer: config[type] || config.default }
+        ]);
+      }}
+    >
+      <ul>
+        {contents.map(({ id, renderer }) => (
+          <li
+            key={id}
+          >
+            {renderer}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
